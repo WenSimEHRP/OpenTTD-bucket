add305762d2072fb1aeccd954911eb294d51a148
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-05-11 11:59:55 -0700

    Add more jgrpp versions, also calculate hashes

--- scripts/createversions.py
+++ scripts/createversions.py
@@ -2,14 +2,28 @@ import json
 
 
 jgrpp_versions = []
+jgrpp_noarm64_versions = []
+jgrpp_diffname_versions = []
+jgrpp_nobuild_versions = []
 with open("scripts/jgrpp_versions.json", "r") as f:
-    jgrpp_versions = json.load(f)["jgrpp_versions"]
+    data = json.load(f)
+    jgrpp_versions = data["jgrpp_versions"]
+    jgrpp_noarm64_versions = data["noarm64"]
+    jgrpp_diffname_versions = data["diffname"]
+    jgrpp_nobuild_versions = data["nobuild"]
 
 
 def create_json(base_path, version):
     with open(f"{base_path}.json.template", "r") as f:
-        with open(f"{base_path}-{version}.json", "w") as f2:
+        with open(f"bucket/jgrpp/openttd-jgrpp-{version}.json", "w") as f2:
             f2.write(f.read().replace("$VERSION", version))
 
 for version in jgrpp_versions:
-    create_json("bucket/jgrpp/openttd-jgrpp", version)
+    if version in jgrpp_noarm64_versions:
+        create_json("bucket/jgrpp/openttd-jgrpp-noarm64", version)
+    elif version in jgrpp_diffname_versions:
+        create_json("bucket/jgrpp/openttd-jgrpp-diffname", version)
+    elif version in jgrpp_nobuild_versions:
+        pass
+    else:
+        create_json("bucket/jgrpp/openttd-jgrpp", version)
