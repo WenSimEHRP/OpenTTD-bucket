498de69344c024006d42a6dc7221b8a4d3b6fb21
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-05-11 17:32:17 -0700

    Add: more legacy versions

--- scripts/legacy_versions/createversions.py
+++ scripts/legacy_versions/createversions.py
@@ -1,29 +1,48 @@
 import json
+import os
+
+bucket = "bucket"
+folders = [
+    "openttd-jgrpp",
+    "openttd",
+]
 
 
-jgrpp_versions = []
-jgrpp_noarm64_versions = []
-jgrpp_diffname_versions = []
-jgrpp_nobuild_versions = []
-with open("scripts/jgrpp_versions.json", "r") as f:
-    data = json.load(f)
-    jgrpp_versions = data["jgrpp_versions"]
-    jgrpp_noarm64_versions = data["noarm64"]
-    jgrpp_diffname_versions = data["diffname"]
-    jgrpp_nobuild_versions = data["nobuild"]
+# delete all json files in the desiginated folder
+for folder in folders:
+    for file in os.listdir(f"{bucket}/{folder}"):
+        if file.endswith(".json"):
+            os.remove(f"{bucket}/{folder}/{file}")
 
 
-def create_json(base_path, version):
-    with open(f"{base_path}.json.template", "r") as f:
-        with open(f"bucket/jgrpp/openttd-jgrpp-{version}.json", "w") as f2:
-            f2.write(f.read().replace("$VERSION", version))
+def create_jsons(name):
 
-for version in jgrpp_versions:
-    if version in jgrpp_noarm64_versions:
-        create_json("bucket/jgrpp/openttd-jgrpp-noarm64", version)
-    elif version in jgrpp_diffname_versions:
-        create_json("bucket/jgrpp/openttd-jgrpp-diffname", version)
-    elif version in jgrpp_nobuild_versions:
-        pass
-    else:
-        create_json("bucket/jgrpp/openttd-jgrpp", version)
+    def create_json(template_path, type, version):
+        with open(f"{template_path}-{type}.json.template", "r") as f:
+            with open(f"{template_path}-{version}.json", "w") as f2:
+                f2.write(f.read().replace("$VERSION", version))
+
+    with open(f"scripts/legacy_versions/{name}-versions.json", "r") as f:
+        data = json.load(f)
+        versions = data["versions"] if "versions" in data else []
+        nowin64 = data["nowin64"] if "nowin64" in data else []
+        noarm64 = data["noarm64"] if "noarm64" in data else []
+        diffname = data["diffname"] if "diffname" in data else []
+        nobuild = data["nobuild"] if "nobuild" in data else []
+
+    for index, version in enumerate(versions):
+        base_path = f"{bucket}/{name}/{name}"
+        if version in nowin64:
+            create_json(base_path, "nowin64", version)
+        elif version in noarm64:
+            create_json(base_path, "noarm64", version)
+        elif version in diffname:
+            create_json(base_path, "diffname", version)
+        elif version not in nobuild:
+            create_json(base_path, "normal", version)
+        else:
+            continue
+
+
+for folder in folders:
+    create_jsons(folder)
